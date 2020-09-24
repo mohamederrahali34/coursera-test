351a02e47bed45e4f16d6d8b7639ab37e2c54ef6
mohamederrahali34 <66528717+mohamederrahali34@users.noreply.github.com>
2020-09-24 10:24:03 +0100

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,11 +1,43 @@
 (function {
 	'use strict';
-	angular.module("LunchCheck",[])
-			  .controller("LunchCheckController",function($scope){
-			    $scope.name="ERRAHALI";
-				var Name=document.getElementById("message");
-				Name.innerHTML="My name is Mohamed "+$scope.name;
-			  });
+			
+		angular.module('LunchCheck',[]).controller('LunchCheckController',function($scope){
+				
+			  $scope.count=function(){
+			  $scope.message=function(){
+			  if(countItems()==0) {
+			  alert("Please enter data first");
+			  return "";}
+			  else if(countItems()<=3){
+			  return "Enjoy!";
+			  }
+			  else return "Too much !!";
+			  }
+			  };
+			  
+			  
+			  function isEmpty(array){
+			  for(var i=0;i<array.length;i++){
+				if(array[i]!="")  return false ;
+			  }
+			  return true;
+			  }
+			  function displayMessage(){
+			  var messageOutput=document.getElementById("msg");
+			  messageOutput.innerHTML(message);
+			  }
+			  function countItems(){
+				 var inputItem=document.getElementById("lunch-menu");
+		        var str=inputItem.value;
+				var strItems=str.split(',');
+				if(isEmpty(strItems)){
+					
+					return 0 ;
+				}
+				else {
+				return strItems.length;}
+			  }
+	});
 	
 });
 ()
